6bcdd8aec098a0f747a2b7c56ec9b65808d83f91
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-04 19:05:51 +0100

    feat: add index and contact route to submit contact message

---
+++ src/routes/contact.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const contactFormValidation = require("../middlewares/contactFormValidation");
+const controllers = require("../controllers/index");
+
+const router = Router();
+
+router.post("/", contactFormValidation, controllers.contact.submitContactForm);
+
+module.exports = router;
+
+// finish off contact form validation
+// submitContactForm - controller do tomorrow
