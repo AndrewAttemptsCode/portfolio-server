becf398a56a8df561d3b1fb9f09d58c15995d12a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-05 17:08:22 +0100

    feat: add nodemailer setup to submitContactForm controller logic

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,5 +1,42 @@
-const submitContactForm = (req, res) => {
-  // TODO nodemailer
+const nodemailer = require("nodemailer");
+const { validationResult } = require("express-validator");
+
+const submitContactForm = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.GOOGLE_APP_PASSWORD,
+    },
+  });
+
+  const emailMessage = {
+    from: {
+      name: "Portfolio Contact",
+      address: process.env.USER_EMAIL,
+    },
+    to: process.env.USER_EMAIL,
+    replyTo: email,
+    subject: `New contact from: ${name}`,
+    text: message,
+    html: `<p style="white-space: pre-wrap;">${message}</p>`,
+  };
+
+  try {
+    await transporter.sendMail(emailMessage);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error("sendMail Error:", err);
+    res.status(500).json({ success: false });
+  }
 };
 
 module.exports = {
